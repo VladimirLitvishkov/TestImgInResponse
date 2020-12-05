4768b63fa9b9506c8af0bb3385dc5044f226fb73
Vladimir <v_litva@mail.ru>
2020-12-05 23:12:27 +0200

    test

--- TestEmailRespons/src/main/java/test/controller/Controller.java
+++ TestEmailRespons/src/main/java/test/controller/Controller.java
@@ -19,7 +19,7 @@ public class Controller {
 	@GetMapping("/response")
 	public void response(HttpServletRequest request, HttpServletResponse response) {
 		BufferedImage pixel = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-		pixel.setRGB(0, 0, (0xFF));
+//		pixel.setRGB(0, 0, (0xFF));
 		response.setContentType("image/png");
 		OutputStream os;
 		try {
