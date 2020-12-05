c6b7eb85508bf37ae94b04c38d47e25d5867c400
Vladimir <v_litva@mail.ru>
2020-12-05 23:00:33 +0200

    test

--- TestEmailRespons/src/main/java/test/controller/Controller.java
+++ TestEmailRespons/src/main/java/test/controller/Controller.java
@@ -1,6 +1,12 @@
 package test.controller;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	
 	@GetMapping("/response")
-	public BufferedImage response() {
+	public void response(HttpServletRequest request, HttpServletResponse response) {
 		BufferedImage pixel = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-//		pixel.setRGB(0, 0, (0xFF));
-		return pixel;
+		pixel.setRGB(0, 0, (0xFF));
+		response.setContentType("image/png");
+		OutputStream os;
+		try {
+			os = response.getOutputStream();
+			ImageIO.write(pixel, "png", os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
