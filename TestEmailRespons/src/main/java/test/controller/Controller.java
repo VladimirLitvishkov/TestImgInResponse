ed900b291b4d7a7de98779a20fdaa0e76e783653
Vladimir <v_litva@mail.ru>
2020-12-05 22:30:05 +0200

    test

--- TestEmailRespons/src/main/java/test/controller/Controller.java
+++ TestEmailRespons/src/main/java/test/controller/Controller.java
@@ -1,5 +1,7 @@
 package test.controller;
 
+import java.awt.image.BufferedImage;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	
 	@GetMapping("/response")
-	public void response() {
+	public BufferedImage response() {
+		BufferedImage pixel = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+		pixel.setRGB(0, 0, (0xFF));
+		return pixel;
 	}
 
 }
