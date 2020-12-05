945f2dadbede21826b1c41a2e8d747b17c60ca45
Vladimir <v_litva@mail.ru>
2020-12-05 22:02:55 +0200

    test

---
+++ TestEmailRespons/src/main/java/test/TestEmailResponsApplication.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEmailResponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEmailResponsApplication.class, args);
+	}
+
+}
